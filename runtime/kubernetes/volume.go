f74d4d01587a570ec198d8b16e31423b442f14d1
Jordan Brockopp <jordan.brockopp@target.com>
2020-03-10 10:57:00 -0500

    feat(kubernetes): add logic to remove resources (#8)

--- runtime/kubernetes/volume.go
+++ runtime/kubernetes/volume.go
@@ -17,7 +17,7 @@ import (
 
 // CreateVolume creates the pipeline volume.
 func (c *client) CreateVolume(ctx context.Context, b *pipeline.Build) error {
-	logrus.Tracef("creating volume for pipeline %s", b.ID)
+	logrus.Tracef("creating volume for pod %s", b.ID)
 
 	// create the volume for the pod
 	//
@@ -51,7 +51,7 @@ func (c *client) CreateVolume(ctx context.Context, b *pipeline.Build) error {
 
 // InspectVolume inspects the pipeline volume.
 func (c *client) InspectVolume(ctx context.Context, b *pipeline.Build) ([]byte, error) {
-	logrus.Tracef("inspecting volume for pipeline %s", b.ID)
+	logrus.Tracef("inspecting volume for pod %s", b.ID)
 
 	// marshal the volume information from the pod
 	bytes, err := json.Marshal(c.Pod.Spec.Volumes)
@@ -64,10 +64,16 @@ func (c *client) InspectVolume(ctx context.Context, b *pipeline.Build) ([]byte, 
 
 // RemoveVolume deletes the pipeline volume.
 //
-// TODO: research this
-//
-// Currently, this is a no-op because in Kubernetes the
-// network lives and dies with the pod it's attached to.
+// Currently, this is comparable to a no-op because in Kubernetes the
+// volume lives and dies with the pod it's attached to. However, Vela
+// uses it to cleanup the volume definition for the pod.
 func (c *client) RemoveVolume(ctx context.Context, b *pipeline.Build) error {
+	logrus.Tracef("removing volume for pod %s", b.ID)
+
+	// remove the volume definition from the pod spec
+	//
+	// https://pkg.go.dev/k8s.io/api/core/v1?tab=doc#PodSpec
+	c.Pod.Spec.Volumes = []v1.Volume{}
+
 	return nil
 }
