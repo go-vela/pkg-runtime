301529af29f61eef890eefd2a32f86a384bd22e0
Jordan Brockopp <jordan.brockopp@target.com>
2020-03-20 09:00:20 -0500

    feat(runtime): add exported CLI flags (#21)

--- cmd/vela-runtime/main.go
+++ cmd/vela-runtime/main.go
@@ -5,10 +5,10 @@
 package main
 
 import (
+	"log"
 	"os"
 	"time"
 
-	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 
 	_ "github.com/joho/godotenv/autoload"
@@ -37,60 +37,12 @@ func main() {
 
 	// Package Flags
 
-	app.Flags = []cli.Flag{
+	app.Flags = flags()
 
-		cli.StringFlag{
-			EnvVar: "VELA_LOG_LEVEL,RUNTIME_LOG_LEVEL",
-			Name:   "log.level",
-			Usage:  "set log level - options: (trace|debug|info|warn|error|fatal|panic)",
-			Value:  "info",
-		},
-		cli.StringFlag{
-			EnvVar: "VELA_PIPELINE_CONFIG,PIPELINE_CONFIG",
-			Name:   "pipeline.config",
-			Usage:  "path to pipeline configuration file",
-			Value:  "testdata/steps.yml",
-		},
-
-		// Compiler Flags
-
-		cli.BoolFlag{
-			EnvVar: "VELA_COMPILER_GITHUB,COMPILER_GITHUB",
-			Name:   "github.driver",
-			Usage:  "github compiler driver",
-		},
-		cli.StringFlag{
-			EnvVar: "VELA_COMPILER_GITHUB_URL,COMPILER_GITHUB_URL",
-			Name:   "github.url",
-			Usage:  "github url, used by compiler, for pulling registry templates",
-		},
-		cli.StringFlag{
-			EnvVar: "VELA_COMPILER_GITHUB_TOKEN,COMPILER_GITHUB_TOKEN",
-			Name:   "github.token",
-			Usage:  "github token, used by compiler, for pulling registry templates",
-		},
-
-		// Runtime Flags
-
-		cli.StringFlag{
-			EnvVar: "VELA_RUNTIME_DRIVER,RUNTIME_DRIVER",
-			Name:   "runtime.driver",
-			Usage:  "name of runtime driver to use",
-		},
-		cli.StringFlag{
-			EnvVar: "VELA_RUNTIME_CONFIG,RUNTIME_CONFIG",
-			Name:   "runtime.config",
-			Usage:  "path to runtime configuration file",
-		},
-		cli.StringFlag{
-			EnvVar: "VELA_RUNTIME_NAMESPACE,RUNTIME_NAMESPACE",
-			Name:   "runtime.namespace",
-			Usage:  "name of namespace for runtime configuration (kubernetes runtime only)",
-		},
-	}
+	// Package Start
 
 	err := app.Run(os.Args)
 	if err != nil {
-		logrus.Fatal(err)
+		log.Fatal(err)
 	}
 }
