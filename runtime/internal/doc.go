17b0a997e7d08e1f4a7537d2068d7fe293c73db5
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-04-05 19:46:32 -0500

    feat: add internal pkg structure

---
+++ runtime/internal/doc.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+// Package internal provides a collection of internal
+// packages used for the Vela runtime environments.
+//
+// Usage:
+//
+// 	import "github.com/go-vela/pkg-runtime/runtime/internal"
+package internal
