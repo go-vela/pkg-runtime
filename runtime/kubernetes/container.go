37318a22f6ca4e3cb7eee57411cbce3816e218fa
Jordan <Jordan.Brockopp@target.com>
2020-09-17 09:47:36 -0500

    docs: fix typos in comments

--- runtime/kubernetes/container.go
+++ runtime/kubernetes/container.go
@@ -101,7 +101,7 @@ func (c *client) RemoveContainer(ctx context.Context, ctn *pipeline.Container) e
 	return nil
 }
 
-// RunContainer creates and start the pipeline container.
+// RunContainer creates and starts the pipeline container.
 func (c *client) RunContainer(ctx context.Context, ctn *pipeline.Container, b *pipeline.Build) error {
 	logrus.Tracef("running container %s", ctn.ID)
 
@@ -207,7 +207,7 @@ func (c *client) RunContainer(ctx context.Context, ctn *pipeline.Container, b *p
 	return nil
 }
 
-// SetupContainer pulls the image for the pipeline container.
+// SetupContainer prepares the image for the pipeline container.
 func (c *client) SetupContainer(ctx context.Context, ctn *pipeline.Container) error {
 	logrus.Tracef("setting up for container %s", ctn.ID)
 
