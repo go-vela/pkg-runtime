94bb5eeced7d60d8e92bb4ea2907e0a1848f6cc7
Jordan <Jordan.Brockopp@target.com>
2021-04-16 11:12:21 -0500

    docs: add clarifying comments for api version

--- runtime/docker/docker.go
+++ runtime/docker/docker.go
@@ -12,8 +12,22 @@ import (
 	mock "github.com/go-vela/mock/docker"
 )
 
-// expected version for the Docker API.
-const version = "1.40"
+// nolint: godot // ignore comment ending in a list
+//
+// Version represents the supported Docker API version for the mock.
+//
+// The Docker API version is pinned to ensure compatibility between the
+// Docker API and client. The goal is to maintain n-1 compatibility.
+//
+// The maximum supported Docker API version for the client is here:
+//
+// https://docs.docker.com/engine/api/#api-version-matrix
+//
+// For example (use the compatibility matrix above for reference):
+//
+// * the Docker version of v20.10 has a maximum API version of v1.41
+// * to maintain n-1, the API version is pinned to v1.40
+const Version = "v1.40"
 
 type config struct {
 	// specifies a list of privileged images to use for the Docker client
@@ -70,7 +84,7 @@ func New(opts ...ClientOpt) (*client, error) {
 	// but this ensures the version of client being used
 	//
 	// https://godoc.org/github.com/docker/docker/client#WithVersion
-	_ = docker.WithVersion(version)(_docker)
+	_ = docker.WithVersion(Version)(_docker)
 
 	// set the Docker client in the runtime client
 	c.Docker = _docker
